3c2e9ab8caf324e143871d7dff420788f506b424
bryr0 <xx.bryan.xx@msn.com>
2020-08-03 11:40:56 -0500

    update django 2 to 3

--- info/urls.py
+++ info/urls.py
@@ -1,7 +1,7 @@
 """info URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,18 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.conf.urls import url, include
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
+from django.conf import settings
+from app.views import index,upload,graphic
+from django.conf.urls.static import static
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+
+# from django.contrib import admin
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^info/', include(('apps.info.urls','info'), namespace='infohome')),
-    url('', include(('apps.info.urls','info'), namespace='info')),
-    # url(r'^info/index/', include(('apps.info.urls','index'), namespace='index')),
-    #url(r'^$','apps.info.views.index'),
-    #url(r'^$', include(('apps.info.views.index','default'), namespace='default')),
+	path('admin/', admin.site.urls),
+	url(r'^upload', upload),
+	url(r'^graphic', graphic),
+	url(r'^index', index),
+    url(r'^$', index),
 ]
 
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
 start chrome http://localhost/
-python manage.py runserver
+python manage.py runserver 0.0.0.0:80
